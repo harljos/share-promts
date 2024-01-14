b415185cd31d79eb4631e1c50fb1e1e5b94886eb
unknown <joseph.harley02@gmail.com>
2024-01-14 12:47:44 -0500

    created createPrompt function

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -14,7 +14,23 @@ const CreatePrompt = () => {
     });
 
     const createPrompt = async (e) => {
+      e.preventDefault();
+      setSubmitting(true);
 
+      try {
+        const response = await fetch("api/prompt/new", {
+          method: 'POST',
+          body: JSON.stringify({
+            prompt: post.prompt,
+            userId: session?.user.id,
+            tag: post.tag
+          })
+        });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setSubmitting(false);
+      }
     }
 
   return (
